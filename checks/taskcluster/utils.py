776e67f79007716fadcd348ab84fff18fbdd1400
bsieber-mozilla <53873986+bsieber-mozilla@users.noreply.github.com>
2021-09-15 18:04:10 -0700

    Update utils.py

--- checks/taskcluster/utils.py
+++ checks/taskcluster/utils.py
@@ -5,7 +5,7 @@ def options_from_params(root_url, client_id, access_token, certificate):
     return {
         "rootUrl": root_url,
         "credentials": (
-            {"clientId": client_id.rstrip(), "accessToken": access_token}
+            {"clientId": client_id.rstrip(), "accessToken": access_token.rstrip()}
             if client_id and access_token
             else {"certificate": certificate}
         ),
